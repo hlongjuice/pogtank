ace5e6111d5f21fdaa0d6929e8cb4b5907234728
hlongjuice <isis.juicy@gmail.com>
2018-05-17 17:34:33 +0700

    pc add wHoops

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,6 +48,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        $whoops = new \Whoops\Run;
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+        $whoops->register();
         return parent::render($request, $exception);
     }
 }
