aa0549fd61610b05592181b9feb6daa8b4f7d93b
hlongjuice <isis.juicy@gmail.com>
2018-06-15 15:20:37 +0700

    Pc Export V0.3

--- app/Http/Controllers/Web/Export/Project/ExportPorlor4Controller.php
+++ app/Http/Controllers/Web/Export/Project/ExportPorlor4Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web\Export\Project;
 
 
 use App\Http\Controllers\Admin\Project\Porlor4JobController;
+use App\Http\Controllers\Others\NumberThaiController;
 use App\Http\Controllers\Others\ThaiDateController;
 use App\Models\Admin\Project\Porlor4;
 use App\Models\Admin\Project\Porlor4Job;
@@ -27,21 +28,21 @@ class ExportPorlor4Controller extends Controller
             'porlor4.part'
         ])->where('id', $root_job_id)->first();
         $rootJob->calculated_child_job = (new Porlor4JobController)->getAllChildJobs($porlor4_id, $root_job_id);
-//        Excel::create('test', function (LaravelExcelWriter $excel) use ($rootJob) {
-//            $excel->sheet($rootJob->name, function ($sheet) use ($rootJob) {
-//                $row =1;
-//                $this->setSheetStyles($sheet);
-//                foreach ($rootJob->calculated_child_job as $childJob) {
-//                    $this->setHeaders($sheet, $rootJob, $childJob,$row);
-//                    $this->setContents($sheet,$rootJob,$childJob,$row);
-//                    $row++;
-//                }
-//            });
-//        })->export('xls');
+        Excel::create('test', function (LaravelExcelWriter $excel) use ($rootJob) {
+            $excel->sheet($rootJob->name, function ($sheet) use ($rootJob) {
+                $row =1;
+                $this->setSheetStyles($sheet);
+                foreach ($rootJob->calculated_child_job as $childJob) {
+                    $this->setHeaders($sheet, $rootJob, $childJob,$row);
+                    $this->setContents($sheet,$rootJob,$childJob,$row);
+                    $row++;
+                }
+            });
+        })->export('xls');
 //        return response()->json($rootJob);
     }
 
-    public function setHeaders($sheet,$rootJob,$childJob,&$row){
+    public function setHeaders(LaravelExcelWorksheet $sheet,$rootJob,$childJob,&$row){
         //Header Table
         //Porlor 4 Page
         $sheet->cell('J'.$row, function (CellWriter $cell) use ($childJob) {
@@ -149,16 +150,63 @@ class ExportPorlor4Controller extends Controller
                 $cell->setValue('หน่วย : บาท');
                 $cell->setAlignment('right');
             });
-            $this->setTableHeaders($sheet,$rootJob,$childJob,$row);
         }
+        $this->setTableHeaders($sheet,$rootJob,$childJob,$row);
     }
-    public function setTableHeaders($sheet,$rootJob,$childJob,&$row){
+    public function setTableHeaders(LaravelExcelWorksheet $sheet,$rootJob,$childJob,&$row){
+        $row++;
+        //Order Number
+        $sheet->mergeCells('A'.$row.':A'.($row+1));
+        $sheet->cell('A'.$row,'ลำดับที่');
+        //Job Name
+        $sheet->mergeCells('B'.$row.':B'.($row+1));
+        $sheet->cell('B'.$row,'รายการ');
+        //Quantity
+        $sheet->mergeCells('C'.$row.':C'.($row+1));
+        $sheet->cell('C'.$row,'จำนวน');
+        //Unit
+        $sheet->mergeCells('D'.$row.':D'.($row+1));
+        $sheet->cell('D'.$row,'หน่วย');
+        //Item Price
+        // -- Header
+        $sheet->mergeCells('E'.$row.':F'.$row);
+        $sheet->cell('E'.$row,'ค่าวัสดุ');
+        // -- Price Per Unit
+        $sheet->cell('E'.($row+1),'ราคา/หน่วย');
+        // -- Total Price
+        $sheet->cell('F'.($row+1),'จำนวนเงิน');
+        //Wage
+        // -- Header
+        $sheet->mergeCells('G'.$row.':H'.$row);
+        $sheet->cell('G'.$row,'ค่าแรงงาน');
+        // -- Price Per Unit
+        $sheet->cell('G'.($row+1),'ราคา/หน่วย');
+        // -- Total Price
+        $sheet->cell('H'.($row+1),'จำนวนเงิน');
+        //Sum Total Price Wage
+        $sheet->cell('I'.$row,'รวม');
+        $sheet->cell('I'.($row+1),'ค่าวัสดุและค่าแรงงาน');
+        //Etc..
+        $sheet->mergeCells('J'.$row.':J'.($row+1));
+        $sheet->cell('J'.$row,'หมายเหตุ');
+
+
+        $sheet->row($row,function(CellWriter $row){
+            $row->setValignment('center');
+            $row->setAlignment('center');
+        });
+        // Next Row
+        $sheet->row($row+1,function(CellWriter $row){
+            $row->setValignment('center');
+            $row->setAlignment('center');
+        });
+
 
     }
 
     public function setContents($sheet,$rootJob,$childJob,&$row)
     {
-
+        $row++;
     }
 
     public function setSheetStyles(LaravelExcelWorksheet $sheet)
